4e643176ccacb75aaaf5c61cdbd5fba1e2026eea
Adam Ross Russell and Dustin Davis <student@epicodus.com>
2016-01-25 08:20:25 -0800

    Setup initial directory and files

---
+++ spec/specs.js
@@ -0,0 +1,14 @@
+describe('number', function() {
+  it("is false if user enters a number less than one", function() {
+    expect(number(0)).to.equal(false);
+  });
+  it("is false if user enters a non number", function() {
+    expect(number("brad")).to.equal("NaN");
+  });
+  it("returns 'ping' if number is divisible by 3", function() {
+    expect(number(3)).to.equal("ping");
+  });
+  it("return 'pong' if number is divisible by 5", function() {
+    expect(number(10)).to.equal("pong");
+  });
+});
